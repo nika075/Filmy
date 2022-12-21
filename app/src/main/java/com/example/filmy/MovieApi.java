0f70dd2f77d3d1db7cecbc5f24a964712ec474b7
nika075 <dominika220110ika19@gmail.com>
2022-12-21 14:37:03 +0100

    new json

--- app/src/main/java/com/example/filmy/MovieApi.java
+++ app/src/main/java/com/example/filmy/MovieApi.java
@@ -5,6 +5,6 @@ import retrofit2.http.GET;
 
 public interface MovieApi {
 
-    @GET("v3/9c2968bd-7d68-46c8-95fa-c3fecaf6ff62")
+    @GET("v3/1707314f-7a51-4e5c-aa02-be8aae9d4b47")
     Call<JSONResponse> getMovies();
 }
